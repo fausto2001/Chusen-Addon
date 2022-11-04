caa934b5a3bce8e98592ef43b151405df899b831
fausto <60607777+fausto2001@users.noreply.github.com>
2022-11-04 11:40:51 -0300

    Update QuickMerchant.lua
    Quick update to fit NPC ID to Brassbolt Mechawrench's.

--- Chusen-Addon/QuickMerchant.lua
+++ Chusen-Addon/QuickMerchant.lua
@@ -6,7 +6,7 @@ local function eventHandler(self, event, ...)
 --BIG split to only get NPC ID.
 ---@diagnostic disable-next-line: param-type-mismatch
     local npcID = select(2, strsplit(" ",(select(2, strsplit(":",(UnitName("target")..": "..select(6, strsplit("-", UnitGUID("target")))))))));
-    if npcID == EdnaMullbyID then
+    if npcID == BrassboltMechawrenchID then
         PlaySound(416);
         SelectGossipOption(GetNumGossipOptions());
     end
